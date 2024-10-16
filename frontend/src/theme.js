ade8432d9c03b1313e5b3f8acd03989e8079c0ba
vmazurova <vikimazurova@gmail.com>
2024-10-16 14:08:31 +0200

    frontend 1.0.5

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -3,24 +3,24 @@ import { extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
   colors: {
     primary: {
-      500: '#9b59b6',  // Fialová barva
-      600: '#8e44ad',  // Tmavší fialová
+      500: '#9b59b6',  
+      600: '#8e44ad', 
     },
     background: {
       gradient: 'linear-gradient(180deg, rgba(27,27,54,1) 0%, rgba(19,19,42,1) 50%, rgba(11,11,33,1) 100%)', // Hladký přechod pozadí
-      100: '#1B1B36',  // Tmavé pozadí
-      200: '#13132A',  // Středně tmavé
-      300: '#0B0B21',  // Nejsytější černé tóny
+      100: '#1B1B36',  
+      200: '#13132A', 
+      300: '#0B0B21',  
     },
     text: {
-      primary: '#FFFFFF',  // Bílý text
-      secondary: '#AAB3D1',  // Světlejší šedý text
+      primary: '#FFFFFF',  
+      secondary: '#AAB3D1',  
     },
   },
   styles: {
     global: {
       body: {
-        bg: "background.gradient",  // Hladký přechod pozadí
+        bg: "background.gradient",  
         color: "text.primary",
       },
     },
